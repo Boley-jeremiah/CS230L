72aeb105c41bfbf3a4f6c1e71b39abd04223b767
Boley-jeremiah <job00006@mix.wvu.edu>
2024-03-04 22:17:15 -0500

    commited out export tag for cards.js

--- src/Cards.js
+++ src/Cards.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 <div class="card-group">
     <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to ad ditional content. This content is a little bit longer.</p>
@@ -12,7 +12,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
     </div>
 
     <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -21,7 +21,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
     </div>
 
     <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -30,4 +30,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
     </div>
 </div>
 
-export default Cards;
+//export default Cards;
