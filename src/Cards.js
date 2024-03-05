94208e1ea74b8f3972ab10c524f05f6fb954aa9a
Boley-jeremiah <job00006@mix.wvu.edu>
2024-03-04 22:12:03 -0500

    Try 3 Part 2

--- src/Cards.js
+++ src/Cards.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 <div class="card-group">
     <div class="card">
         <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to ad ditional content. This content is a little bit longer.</p>
                 <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
     </div>
