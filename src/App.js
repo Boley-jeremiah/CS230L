93086f6010d45cee7fd9d139c710706e0db6f254
Boley-jeremiah <job00006@mix.wvu.edu>
2024-03-13 11:17:27 -0400

    done???

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+           <navBar />
           <h1>CS 230L</h1>
           <h2>Section-02</h2>
           <p>WVU ID: 8000351750</p>
           <p>Hi I am Jeremiah</p>
+          <Cards/>
     </div>
   );
 }
